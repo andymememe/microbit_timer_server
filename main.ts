2586954df64ed812e3861dd39ef11347b013b295
Liang Yu Chen <andymememe@users.noreply.github.com>
2021-01-16 12:00:34 +0800

    Add: Timer and Sound

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    Planted = 0
+    Timer = 5
+    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
+    Planted = 1
+    music.stopMelody(MelodyStopOptions.All)
 })
+let Timer = 0
+let Planted = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
+Planted = 0
+Timer = 5
+music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.ForeverInBackground)
 basic.forever(function () {
-	
+    if (Planted == 0) {
+        basic.showNumber(Timer)
+        basic.pause(1000)
+        Timer += -1
+        if (Timer < 0) {
+            Timer = 5
+        }
+    } else {
+    	
+    }
 })
